9d1329c6f1339a6562b8926fdd037179a909a010
ES Alexander <sandman.esalexander@gmail.com>
2020-07-28 22:08:40 +1000

    pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name='pythonic-cv',
-    version='1.0.0',
+    version='1.0.1',
     author='ES-Alexander',
     author_email='sandman.esalexander@gmail.com',
     description='Performant pythonic wrapper of unnecessarily painful opencv functionality',
