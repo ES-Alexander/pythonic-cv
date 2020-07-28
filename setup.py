830ad9f3ac4d2475ded3972bdbc25e52c6e45bc2
ES Alexander <sandman.esalexander@gmail.com>
2020-07-28 21:12:54 +1000

    pip

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pythonic-cv',
+    version='0.1.0',
+    author='ES-Alexander',
+    author_email='sandman.esalexander@gmail.com',
+    description='Performant pythonic wrapper of unnecessarily painful opencv functionality',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ES-Alexander/pythonic-cv',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
